98c9a06bd1e3aec25d0381e421c59d201781afed
Davi Gouveia <75867613+davitgouveia@users.noreply.github.com>
2023-06-03 19:16:40 -0300

    Update App.js

--- spotify-gathering-app/src/App.js
+++ spotify-gathering-app/src/App.js
@@ -21,7 +21,7 @@ function downloadStringAsTxt(stringContent, fileName) {
 
 function App() {
   const CLIENT_ID = credentials.clientID
-  const REDIRECT_URI = "https://spotify-data-collection-app-hqyh.vercel.app/"
+  const REDIRECT_URI = "https://spotify-data-collection-app.vercel.app/"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
   const SCOPE = "user-top-read"
